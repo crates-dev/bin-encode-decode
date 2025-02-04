632048c31f332842fdee68ba914a159eea510e2e
尤雨东 <root@ltpp.vip>
2025-02-04 10:29:30 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git remote add ltpp ssh://git@git.ltpp.vip:66/root/bin-encrypt-decrypt.git;
+git remote add jihulab git@jihulab.com:ltpp-universe/bin-encrypt-decrypt.git;
 git remote add github git@github.com:ltpp-universe/bin-encrypt-decrypt.git;
 git remote add origin git@github.com:ltpp-universe/bin-encrypt-decrypt.git;
